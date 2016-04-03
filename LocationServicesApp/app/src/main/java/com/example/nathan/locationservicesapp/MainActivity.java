70bc1c62b92ef0f19eee2c8904d7a5ac49c5f831
SamuelColacchia <samuel.colacchia@gordon.edu>
2016-04-03 17:01:37 -0400

    Merged changes

--- LocationServicesApp/app/src/main/java/com/example/nathan/locationservicesapp/MainActivity.java
+++ LocationServicesApp/app/src/main/java/com/example/nathan/locationservicesapp/MainActivity.java
@@ -1,30 +1,95 @@
 package com.example.nathan.locationservicesapp;
 
+import android.location.Location;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.text.BreakIterator;
+
+public class MainActivity extends AppCompatActivity implements OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
+
+    private GoogleApiClient mGoogleApiClient;
+    private Location currentLocation;
+    LocationRequest locationRequest;
+    private String TAG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+//        LocationServices.FusedLocationApi.requestLocationUpdates(locationRequest, this);
+    }
+
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+
+    public void getCurrentLocation(View view) {
+        Log.i(TAG, "Button Clicked");
+        onStart();
+    }
+
+    boolean connecting;
+
+    boolean connected;
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+        connecting =  mGoogleApiClient.isConnecting();
+
+        connected = mGoogleApiClient.isConnected();
+
+        currentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (currentLocation != null) {
+            double lat = currentLocation.getLatitude();
+            double lon = currentLocation.getLongitude();
+
+            TextView locationMessage = (TextView) findViewById(R.id.location_message);
+            locationMessage.setText("Latitude: " + lat + ", Longitude: " + lon);
+
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "Connection Suspended");
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.i(TAG, "Connection failed. Error: " + connectionResult.getErrorCode());
     }
 }
