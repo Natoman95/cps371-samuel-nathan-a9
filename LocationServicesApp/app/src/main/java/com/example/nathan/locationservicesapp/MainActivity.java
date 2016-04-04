ab47b189b8fcb6a5ac33153376ff78d2b7064aac
Nathan Gray <nathan.gray@gordon.edu>
2016-04-03 22:19:47 -0400

    Everything finally works! Includes an automatically refreshing location and a manually refreshed location.

--- LocationServicesApp/app/src/main/java/com/example/nathan/locationservicesapp/MainActivity.java
+++ LocationServicesApp/app/src/main/java/com/example/nathan/locationservicesapp/MainActivity.java
@@ -3,38 +3,31 @@ package com.example.nathan.locationservicesapp;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesClient;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
-import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.FusedLocationProviderApi;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
-import android.support.v4.app.FragmentActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+public class MainActivity extends AppCompatActivity
+        implements OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks,
+        com.google.android.gms.location.LocationListener {
 
-import org.w3c.dom.Text;
-
-import java.text.BreakIterator;
-
-public class MainActivity extends AppCompatActivity implements OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
-
-    private GoogleApiClient mGoogleApiClient;
-    private Location currentLocation;
-    LocationRequest locationRequest;
-    private String TAG;
+    private GoogleApiClient mGoogleApiClient; // Connects to Google's API
+    private Location currentLocation; // Stores the most recent location data
+    private String TAG = "MainActivity"; // Used to identify error messages
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Initialize the API client
         if (mGoogleApiClient == null) {
             mGoogleApiClient = new GoogleApiClient.Builder(this)
                     .addConnectionCallbacks(this)
@@ -42,44 +35,59 @@ public class MainActivity extends AppCompatActivity implements OnConnectionFaile
                     .addApi(LocationServices.API)
                     .build();
         }
-//        LocationServices.FusedLocationApi.requestLocationUpdates(locationRequest, this);
     }
 
+    @Override
+    // Called when the Client is initialized
     protected void onStart() {
+        Log.i(TAG, "Client Connecting");
         super.onStart();
         mGoogleApiClient.connect();
     }
 
+    @Override
+    // Called when Client is finished
     protected void onStop() {
+        Log.i(TAG, "Client Disconnecting");
         super.onStop();
         mGoogleApiClient.disconnect();
     }
 
+    // Called when the Button is pressed
     public void getCurrentLocation(View view) {
         Log.i(TAG, "Button Clicked");
-        onStart();
-    }
 
-    boolean connecting;
-
-    boolean connected;
-
-    @Override
-    public void onConnected(Bundle bundle) {
-
-        connecting =  mGoogleApiClient.isConnecting();
-
-        connected = mGoogleApiClient.isConnected();
-
+        // Get the most recent location
         currentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+        // If it's not null, then update the view
         if (currentLocation != null) {
             double lat = currentLocation.getLatitude();
             double lon = currentLocation.getLongitude();
 
-            TextView locationMessage = (TextView) findViewById(R.id.location_message);
-            locationMessage.setText("Latitude: " + lat + ", Longitude: " + lon);
-
+            TextView manualLat = (TextView) findViewById(R.id.manual_lat);
+            TextView manualLong = (TextView) findViewById(R.id.manual_long);
+            manualLat.setText("Latitude:   " + lat);
+            manualLong.setText("Longitude:   " + lon);
         }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        // As soon as the Client connects, get the last location
+        currentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+        // Create a location request for the automatically updating data
+        LocationRequest request = LocationRequest.create();
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        request.setInterval(1000); // Poll every second
+        request.setFastestInterval(1000);
+
+        // Create a special instance of the location provider
+        FusedLocationProviderApi fusedLocationProviderApi = LocationServices.FusedLocationApi;
+
+        // Request regular updates from the special location provider using the request object
+        fusedLocationProviderApi.requestLocationUpdates(mGoogleApiClient, request, this);
     }
 
     @Override
@@ -91,5 +99,23 @@ public class MainActivity extends AppCompatActivity implements OnConnectionFaile
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
         Log.i(TAG, "Connection failed. Error: " + connectionResult.getErrorCode());
+    }
+
+    @Override
+    // Called every time the location provider gets new data
+    public void onLocationChanged(Location location) {
+        // Update the current location with the new data
+        currentLocation = location;
+
+        // If it's not null, update the view
+        if (location != null) {
+            double lat = location.getLatitude();
+            double lon = location.getLongitude();
+
+            TextView refreshLat = (TextView) findViewById(R.id.refresh_lat);
+            TextView refreshLong = (TextView) findViewById(R.id.refresh_long);
+            refreshLat.setText("Latitude:   " + lat);
+            refreshLong.setText("Longitude:   " + lon);
+        }
     }
 }
